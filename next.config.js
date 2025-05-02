de67adb2cf3178a7c8acad2ab4ccaf6e4dee9fd8
akitoxiii <akito22622@gmail.com>
2025-05-02 15:27:45 +0900

    fix: disable server components and optimize for static export

--- next.config.js
+++ next.config.js
@@ -12,6 +12,10 @@ const nextConfig = {
   },
   basePath: process.env.NODE_ENV === 'production' ? '/calculator' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/calculator/' : '',
+  experimental: {
+    serverActions: false,
+    serverComponents: false,
+  }
 };
 
 module.exports = nextConfig; 
